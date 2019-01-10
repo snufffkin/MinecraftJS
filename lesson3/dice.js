c4b4ef7dc98ca71927038430e483cc4ccf86d627
Roman <otchepenec42@gmail.com>
2019-01-10 15:51:22 +0300

    Update dice.js

--- lesson3/dice.js
+++ lesson3/dice.js
@@ -2,16 +2,16 @@ var Drone = require('drone'); //stage 1
 
 function dice() { //stage 1
     for (var j = 0; j < 5; j++) {
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < 10; i++) { //stage 4
             var num = Math.ceil(Math.random() * 6) //stage 2
             if (num < 3) { //stage 3
                 this //stage 3
                 .box(42) //stage 3
             } //stage 3
-            this
-            .right()
-        }
-        this
+            this //stage 5
+            .right() //stage 5
+        } //stage 4
+        this 
         .left(10)
         .fwd()
     }
