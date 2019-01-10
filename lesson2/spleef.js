b489677e73b5bdb8dce70bb2e89d06419000a5e9
Roman <otchepenec42@gmail.com>
2019-01-10 13:45:59 +0300

    Update spleef.js

--- lesson2/spleef.js
+++ lesson2/spleef.js
@@ -1,11 +1,11 @@
-var Drone = require('drone');
+var Drone = require('drone'); //stage 1
 
-function spleef() {
-    this
-    .cylinder(10, 1, 10)
-    .up()
-    .cylinder(80, 1, 10)
-    .cylinder0(42, 5, 10)
-}
+function spleef() { //stage 2
+    this //stage 3
+    .cylinder(10, 1, 10) //stage 3
+    .up() //stage 3
+    .cylinder(80, 1, 10) //stage 3
+    .cylinder0(42, 5, 10) //stage 3
+}//stage 2
 
-Drone.extend(spleef)
+Drone.extend(spleef) //stage 4
